fe7d591a8caa4b980bc725fd49e0b47dbc846cf0
goodspeed <hi@gusibi.mobi>
2024-09-28 16:31:39 +0800

    update share image

--- components/MobilePostPreview.vue
+++ components/MobilePostPreview.vue
@@ -1,8 +1,9 @@
 <template>
-    <div ref="mobilePreview" class="mobile-preview w-[390px] min-h-[400px] font-sans bg-gradient-to-br from-blue-200 to-purple-300 flex flex-col items-center p-4">
+    <div ref="mobilePreview" class="mobile-preview w-[390px] min-h-[400px] font-sans bg-gradient-to-br from-red-200 to-purple-300 flex flex-col items-center p-4">
+        <!-- <div class="justify-center text-center p-2 text-2xl text-gray-600 dark:text-gray-400">古思乱讲</div> -->
         <div class="w-full max-w-sm bg-white dark:bg-gray-800 rounded-lg shadow-xl overflow-hidden mb-4">
             <div class="p-6">
-                <h2 class="text-2xl font-bold mb-2 text-gray-900 dark:text-white">{{ post.title || 'No Title' }}</h2>
+                <h2 v-if="!isMemePost(post.labels)" class="text-2xl font-bold mb-2 text-gray-900 dark:text-white">{{ post.title || 'No Title' }}</h2>
                 <div class="flex justify-between items-center">
                     <div class="text-sm text-gray-600 dark:text-gray-400 mb-4">
                         {{ formatDate(post.created_at) }}
@@ -18,10 +19,10 @@
                 </div>
             </div>
         </div>
-        <div class="justify-center text-center p-2 text-sm text-gray-600 dark:text-gray-400">古思乱讲: https://momo.gusibi.mobi </div>
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center p-2">
             <QRCode :value="postUrl" :size="60" level="M" render-as="svg" :margin="0" class="rounded-md overflow-hidden" />
         </div>
+        <div class="justify-center text-center text-xs text-slate-400 dark:text-gray-400">古思乱讲：{{ postUrl }} </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -43,6 +44,9 @@ const postUrl = computed(() => {
     return `${config.public.siteUrl}/blog/${props.post.number}`
 })
 
+const isMemePost = (labels: Array<{ name: string }>): boolean => {
+    return labels.some(label => label.name.toLowerCase() === 'meme')
+}
 
 onMounted(() => {
     console.log('MobilePostPreview mounted, post data:', props.post)
